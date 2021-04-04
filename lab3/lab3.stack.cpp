287e0583331c82b68a9123d21a878ff8323f0682
MaximVorobev <maxv153218@gmail.com>
2021-04-04 14:24:31 +0700

    lab3 stack

---
+++ lab3/lab3.stack.cpp
@@ -0,0 +1,180 @@
+#include <iostream>
+#include <fstream>
+#include <string>
+#include <stdlib.h>
+using namespace std;
+
+struct Pen{
+    string color;
+    double price;
+    string color_ink;
+    double length;
+    void Out();
+};
+
+void Pen::Out(){
+    cout << "\tColor: " << color << endl;
+    cout << "\tPrice: " << price << endl;
+    cout << "\tColor of ink: " << color_ink << endl;
+    cout << "\tLength:" << length << endl;
+}
+
+struct Stack
+{
+    struct Node
+    {
+        Pen data; Node* prev;
+    };
+    Node *top = NULL;
+    int count;
+    bool Push(Pen); // Добавить элемент в стек
+    bool Pop(Pen&); // Удалить элемент из стека
+    void Info();
+};
+
+bool Stack::Push(Pen data){
+    if (!top){
+        top = new Node;
+        top->prev=NULL;
+        count=1;
+    }
+    else{
+        Node *temp;
+        temp=new Node;
+        temp->prev=top;
+        top=temp;
+        count++;
+    }
+    top->data=data;
+    return true;
+}
+
+bool Stack::Pop(Pen& data){
+    if (!top) return false;
+    Node* temp = top->prev;
+    data=top->data;
+    delete top;
+    top = temp;
+    count--;
+    return true;
+}
+
+void Stack::Info(){
+    if(!top)
+        cout << "---> Stack is empty" << endl;
+    else{
+        cout << "Stack info: " << endl;
+        cout << "\tStack size = " << count << endl;
+        top->data.Out();
+        cout << endl;
+    }
+}
+
+void Menu();
+void readFile(Stack&);
+void userPush(Stack&);
+void getOut(Stack&);
+void Clear(Pen&, Stack&);
+
+void Menu(){
+    Stack s;
+    Pen p;
+    readFile(s);
+    int key;
+    do{
+        system("cls");
+        cout << "------------" << endl;
+        cout << "1. Add product in cart" << endl;
+        cout << "2. Get out product from cart" << endl;
+        cout << "3. Clear cart" << endl;
+        cout << "0. Exit" << endl;
+        cout << "------------" << endl;
+        s.Info();
+        cout << "Enter: ";
+        cin >> key;
+
+        switch(key){
+            case 1: userPush(s); break;
+            case 2: getOut(s); break;
+            case 3: Clear(p, s); break;
+            case 0: break;
+            default:  system("cls"); 
+                      cout << "Error data, try again..." << endl; 
+                      system("pause");
+        }
+    }while(key);
+}
+
+void readFile(Stack &stack){
+    Pen pen;
+    fstream data;
+    data.open("data.txt", ios::in);
+    if (!data){
+        cout << "Error open file" << endl;
+        return;
+    }
+    else{
+        while (data >> pen.color >> pen.price >> pen.color_ink >> pen.length){
+            stack.Push(pen);
+        }
+        data.close();
+    }
+}
+
+void userPush(Stack& s){
+    string color, color_ink;
+    double price, length;
+    Pen pen;
+    cout << "Enter color, price, color of ink and length of pen: ";
+    cin >> color >> price >> color_ink >> length;
+    pen = {color, price, color_ink, length};
+    s.Push(pen);
+}
+
+void getOut(Stack& s){
+    Stack temp; // буферный стек
+    Pen top; // верхний элемент стека
+    string color, color_ink;
+    double price, length; 
+    bool flag = true;
+    if (s.count == 0){
+            cout << "No products in cart" << endl;
+            system("pause");
+            return;
+    }
+    cout << "Which product do you want to have? Enter its properties: ";
+    cin >> color >> price >> color_ink >> length;
+    while (flag){
+        top = s.top->data;
+        if ((color == top.color) and (price == top.price) and (color_ink == top.color_ink) and (length == top.length)){
+            s.Pop(top);
+            flag = 0;
+            break;
+        }
+        else if (s.count>0){
+            s.Pop(top);
+            temp.Push(top);
+        }
+        if (s.count == 0) break;
+    }
+    while(temp.count>0){
+        top = temp.top->data;
+        s.Push(top);
+        temp.Pop(top);
+    }
+    if (flag) cout << "No product in the cart" << endl;
+    else cout << "The product found" << endl;
+    system("pause");
+}
+
+void Clear(Pen& pen, Stack &s){
+    while (s.count>0){
+        s.Pop(pen);
+    }
+}
+
+int main()
+{   
+    Menu();
+    return 0;
+}
